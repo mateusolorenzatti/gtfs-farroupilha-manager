ac74975dbf13388eadb090c40931849c25d70243
Mateus Orlandin Lorenzatti <mateusolorenzatti@gmail.com>
2021-03-18 15:34:24 -0300

    Implementado o calculo dos horarios das paradas em relaçao ao atraso com a Parada Inicial

--- static/js/gtfs-fm/new_trip_helper.js
+++ static/js/gtfs-fm/new_trip_helper.js
@@ -4,24 +4,47 @@ $(function() {
     // Crair uma assossiação entre os horários das paradas, para criar uma relação de retardo entre todas
     var horario_inicio_fixo = $('#horario-inicio').val();
 
+    var DEBUG_MODE = false;
+
     $('#horario-inicio').blur(() => {
         horario_inicio = new Date('2021-01-01 ' + $('#horario-inicio').val());
 
         // Conferir se a data é inválida (horario_inicio_fixo)
-        
+        if (isNaN(horario_inicio.getTime())) {  
+            $('#horario-inicio').val(horario_inicio_fixo);
+
+            halfmoon.initStickyAlert({
+                content: "Voltei para o horário que tínhamos no inicio da sessão.",
+                title: "Horário Inválido! ",
+                alertType: "alert-danger",
+            });
+
+            return;
+        }
+
+        td_deltas_tabela = $('.diferenca-tabela');
         td_horarios_tabela = $('.horario-tabela');
 
         horarios = [];
 
-        for(let i = 0; i < td_horarios_tabela.length; i++){
-            // horarios.push(new Date('2021-01-01 ' + td_horarios_tabela[i].innerText))
-            horario = new Date('2021-01-01 ' + td_horarios_tabela[i].innerText);
+        for(let i = 0; i < td_deltas_tabela.length; i++){
+            if(DEBUG_MODE) console.log(" --------------------------------------------------------- ");
 
-            dif = (horario.getTime() - horario_inicio.getTime()) / 1000;
+            horario_inicio = new Date('2021-01-01 ' + $('#horario-inicio').val());
+            if(DEBUG_MODE) console.log(" Horário de Inicio: " + horario_inicio);
 
-            console.log(dif);
+            delta = new Date('2021-01-01 0' + td_deltas_tabela[i].innerText.substring(2));
+            if(DEBUG_MODE) console.log(" Diferença a ser somada: " + delta);
+
+            horario_inicio.setSeconds(horario_inicio.getSeconds() + delta.getSeconds());
+            horario_inicio.setMinutes(horario_inicio.getMinutes() + delta.getMinutes());
+            horario_inicio.setHours(horario_inicio.getHours() + delta.getHours());
+
+            horario = new Date( horario_inicio );
+            if(DEBUG_MODE) console.log(" Novo horário calculado: " + horario);
+
+            // Altera o horário calculado no respectivo marcador
+            td_horarios_tabela[i].innerText = horario.toTimeString().substring(0, 8);
         }
-
-        console.log(horarios);
     });
 });
